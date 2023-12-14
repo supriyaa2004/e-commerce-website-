5843e687ab88be48d0f1f648b62cdf09d6dc2e50
Supriya Singh <129054562+supriyaa2004@users.noreply.github.com>
2023-12-14 19:44:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 
 //esmodule 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname=path.dirname(_filename);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
