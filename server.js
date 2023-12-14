5fd100de0d5c1a52c4605f1a5fce8c4dc376aeaf
Supriya Singh <129054562+supriyaa2004@users.noreply.github.com>
2023-12-14 19:42:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
+import categoryRoutes from "./routes/CategoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
